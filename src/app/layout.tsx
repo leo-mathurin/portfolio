0792fa8afc1236a2041e2ea900576fb7ce0aefe3
Léo Mathurin <leo.mathurin@epitech.eu>
2025-06-21 16:53:48 +0200

    Removed og.png image and updated layout to reference og.jpg instead.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: `${DATA.url}/og.png`,
+        url: `${DATA.url}/og.jpg`,
         width: 1200,
         height: 630,
       },
